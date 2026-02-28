409aa71a62c177a35e1a19a9e11db64393837206
IzaguirreCarlos <carlosizaguirres705@gmail.com>
2026-02-28 17:05:55 -0500

    modificando el static root

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 
 # Archivos estáticos
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
